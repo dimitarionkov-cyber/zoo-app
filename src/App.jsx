96a61fc9a8ba9d392b57ad3e4950d478f2249de6
Dimitar Ionkov <dimitar.ionkov@gmail.com>
2026-05-11 14:52:07 +0300

    feat: add 15 new animals (giants + primates); lazy-load pages to fix bundle size warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,45 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { DataProvider } from './context/DataContext'
 import { MapsProvider } from './context/MapsContext'
 import Layout from './components/Layout/Layout'
 import HomePage from './pages/HomePage'
-import MapPage from './pages/MapPage'
-import AnimalsHubPage from './pages/AnimalsHubPage'
-import AnimalDetailPage from './pages/AnimalDetailPage'
-import SearchPage from './pages/SearchPage'
-import InfoPage from './pages/InfoPage'
-import SettingsPage from './pages/SettingsPage'
+
+const MapPage         = lazy(() => import('./pages/MapPage'))
+const AnimalsHubPage  = lazy(() => import('./pages/AnimalsHubPage'))
+const AnimalsListPage = lazy(() => import('./pages/AnimalsListPage'))
+const AnimalDetailPage = lazy(() => import('./pages/AnimalDetailPage'))
+const SearchPage      = lazy(() => import('./pages/SearchPage'))
+const InfoPage        = lazy(() => import('./pages/InfoPage'))
+const SettingsPage    = lazy(() => import('./pages/SettingsPage'))
+
+function PageLoader() {
+  return (
+    <div className="flex items-center justify-center h-full text-zoo-brown opacity-50 text-sm">
+      Зарежда се…
+    </div>
+  )
+}
 
 export default function App() {
   return (
     <DataProvider>
       <MapsProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="map" element={<MapPage />} />
-            <Route path="animals" element={<AnimalsHubPage />} />
-            <Route path="animals/:id" element={<AnimalDetailPage />} />
-            <Route path="search" element={<SearchPage />} />
-            <Route path="info" element={<InfoPage />} />
-            <Route path="settings" element={<SettingsPage />} />
-          </Route>
-        </Routes>
+        <Suspense fallback={<PageLoader />}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              <Route path="map" element={<MapPage />} />
+              <Route path="animals" element={<AnimalsHubPage />} />
+              <Route path="animals/list" element={<AnimalsListPage />} />
+              <Route path="animals/:id" element={<AnimalDetailPage />} />
+              <Route path="search" element={<SearchPage />} />
+              <Route path="info" element={<InfoPage />} />
+              <Route path="settings" element={<SettingsPage />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
       </MapsProvider>
     </DataProvider>
